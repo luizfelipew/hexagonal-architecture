15283c3d3933afc1196feedd05fb0898dfa23c8e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-11-05 19:57:08 -0300

    criando modulo com as exceptions

---
+++ sistema/src/main/java/conta/sistema/dominio/modelo/NegocioException.java
@@ -0,0 +1,8 @@
+package conta.sistema.dominio.modelo;
+
+public class NegocioException extends RuntimeException {
+
+    public NegocioException(String mensagem) {
+        super(mensagem);
+    }
+}
