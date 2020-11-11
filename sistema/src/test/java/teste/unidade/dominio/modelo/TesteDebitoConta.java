c46de82165ee5cc299d4081622041b0e411e9ee5
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-11-10 21:30:56 -0300

    add testes para credito

--- sistema/src/test/java/teste/unidade/dominio/modelo/TesteDebitoConta.java
+++ sistema/src/test/java/teste/unidade/dominio/modelo/TesteDebitoConta.java
@@ -33,4 +33,38 @@ public class TesteDebitoConta {
             assertEquals(e.getMessage(), "Valor crédito é obrigatório.");
         }
     }
+
+    @Test
+    @DisplayName("Valor credito negativo como obrigatorio")
+    void deveVerificarValorCreditoNegativo() {
+        try {
+            contaValida.creditar(BigDecimal.valueOf(-10));
+            fail("valor crédito obrigatório");
+        } catch (NegocioException e) {
+            assertEquals(e.getMessage(), "Valor crédito é obrigatório.");
+        }
+    }
+
+    @Test
+    @DisplayName("Valor credito zerado como obrigatorio")
+    void deveVerificarValorCreditoZerado() {
+        try {
+            contaValida.creditar(BigDecimal.ZERO);
+            fail("valor crédito obrigatório");
+        } catch (NegocioException e) {
+            assertEquals(e.getMessage(), "Valor crédito é obrigatório.");
+        }
+    }
+
+    @Test
+    @DisplayName("Valor credito acima de zero como obrigatorio")
+    void deveVerificarValorCreditoValido() {
+        try {
+            contaValida.creditar(BigDecimal.ONE);
+            var saldoFinal = cem.add(BigDecimal.ONE);
+            assertEquals(contaValida.getSaldo(), saldoFinal, "Saldo deve bater");
+        } catch (NegocioException e) {
+            fail("Deve creditar com sucesso - " + e.getMessage());
+        }
+    }
 }
